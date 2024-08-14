14c7dead5a922ad2bcec4ee9a66fb16f0c6e48af
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2024-08-15 02:51:34 +0600

    add all main file source folder

--- source/main.js
+++ source/main.js
@@ -1,7 +1,33 @@
 // http server
-const http = require('http');
-const server = http.createServer(function (req,res) {
-res.end("Hello World");
+var http = require('http');
+var server = http.createServer(function (req,res) {
+      res.end("Hello World");
+});
+
+//server.listen(5059);
+//console.log("Server Run Success");
+
+
+//URL Module
+var http = require('http');
+var URL=require('url');
+
+var server = http.createServer(function (req,res) {
+
+      var myURL="http://rabbil.com/blog.html?year=2020&month=july";
+
+      var myURLObj= URL.parse(myURL, true);
+
+      var myHostName=myURLObj.hostname;
+      var myPathName= myURLObj.pathname;
+      var mySearchName= myURLObj.search;
+
+      res.writeHead(200,{'content-type':'text/html'})
+      res.write(myHostName);
+      res.write(myPathName);
+      res.write(mySearchName);
+      res.end();
+
 });
 
 server.listen(5059);
