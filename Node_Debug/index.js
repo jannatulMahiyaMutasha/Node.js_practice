5d1d697a6db70813ea5bfc22df46225c6a9576a3
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2024-08-15 03:03:34 +0600

    node debug code added

---
+++ Node_Debug/index.js
@@ -0,0 +1,18 @@
+var axios=require('axios');
+
+var URL="https://restcountries.com/v3.1/all";
+
+
+axios.get(URL).then(function (response) {
+
+
+    let CountryList=response.data;
+    let StatusCode=response.status;
+
+    console.log(CountryList)
+    console.log(StatusCode)
+
+
+}).catch(function (error){
+    console.log(error)
+})
